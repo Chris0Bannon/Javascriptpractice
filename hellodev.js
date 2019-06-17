8d8a9cfa65b2813a150505ff95fe4d3bc2acf566
Christopher O'Bannon <Crobwan@gmail.com>
2019-06-17 16:23:32 -0500

    see added single line comments

--- hellodev.js
+++ hellodev.js
@@ -1,7 +1,7 @@
-Let NAME = "Chris";
+Let NAME = "Chris"; // This is a single line comment and is invisible.
 Let Time = "4:01PM";
 Time = "4:07PM";
 Time = "4:08PM";
-Let Location = "Savage";
-Let Dogs = "1";
-Let Hungry = "false";
+Let Location = "Savage"; // This is a string Variable since it is characters in quotes.
+Let Dogs = 1; // This is a number varible, there are no quotes and a value.
+Let Hungry = false; // This is boolean variable, no quotes, true or false.
