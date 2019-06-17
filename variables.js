5001cf61dbf1008f54808ff483b7d038e986dffa
Christopher O'Bannon <Crobwan@gmail.com>
2019-06-17 16:59:59 -0500

    played with some variables

--- variables.js
+++ variables.js
@@ -1,1 +1,15 @@
 console.log( 'js' );
+// declare and initialize a variable
+let testVar = 9;
+
+console.log( testVar);
+
+console.log( 'The value of testVar is',testVar );
+
+testVar = 8765;
+
+console.log( 'The value of testVar is',testVar );
+
+testVar = 'some other text';
+
+console.log( 'The value of testVar is',testVar );
