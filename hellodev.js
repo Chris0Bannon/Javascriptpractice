13a287ad847b3ded1cce937961634b9c046c871f
Christopher O'Bannon <Crobwan@gmail.com>
2019-06-17 16:31:24 -0500

    cleaned things up, added const and more variables

--- hellodev.js
+++ hellodev.js
@@ -1,7 +1,13 @@
-Let NAME = "Chris"; // This is a single line comment and is invisible.
+Let NAME = "Chris"; // This is a single line comment and is invisible.;
 Let Time = "4:01PM";
 Time = "4:07PM";
 Time = "4:08PM";
-Let Location = "Savage"; // This is a string Variable since it is characters in quotes.
-Let Dogs = 1; // This is a number varible, there are no quotes and a value.
-Let Hungry = false; // This is boolean variable, no quotes, true or false.
+Let Location = "Savage"; // This is a string Variable since it is characters in quotes.;
+Let Dogs = 1; // This is a number varible, there are no quotes and a value.;
+Let Hungry = false; // This is boolean variable, no quotes, true or false.;
+const petsallowed = 2;
+
+Time = "4:27PM";
+Location = "Home";
+Dogs = 2;
+Hungry = true;
