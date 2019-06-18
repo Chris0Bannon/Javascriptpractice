7c2f698bc713bfb31c7f8df287bcc7b6fe8d6f79
Christopher O'Bannon <Crobwan@gmail.com>
2019-06-18 13:03:38 -0500

    first commit with conditionals

---
+++ conditionals.js
@@ -0,0 +1,14 @@
+console.log( 'js' );
+
+// road trip planner;
+let seatsInCar = 4;
+let passengers = 2;
+
+//check that there is enough room for everyone in the car;
+
+if ( seatsInCar >= passengers){
+  console.log( 'We are ready for the trip!' );
+} // end enough seats
+else {
+  console.log( 'there are not enough seats' );
+} // end not enough seats
