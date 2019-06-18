623106da530a76e817323126dd5f64a8ceb36deb
Christopher O'Bannon <Crobwan@gmail.com>
2019-06-18 11:13:17 -0500

    more practice

---
+++ String.js
@@ -0,0 +1,22 @@
+console.log('js');
+
+let dogName = 'BabaYaga';
+let dogAge = 2;
+
+console.log(dogName);
+//concatenating with a comma for the console. the comma only works in console.log();;
+console.log('The name of the dog is:', dogName);
+console.log('The age of the dog is:', dogAge);
+
+//concatenate text "=";
+let sentance = 'This is a sentance about the Dog named ' + dogName;
+console.log(sentance);
+
+sentance = 'The dog is ' + dogAge + ' years old.';
+console.log( sentance);
+
+dogName = 'Kaya';
+dogAge = 3;
+
+sentance = 'Later on, a different dog is ' + dogAge + ' years old, and is named ' + dogName;
+console.log( sentance );
