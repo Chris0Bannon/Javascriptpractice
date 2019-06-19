a76703fdc2ad6dcbc68954fbd7567ef1405786a8
Christopher O'Bannon <Crobwan@gmail.com>
2019-06-19 14:11:33 -0500

    first commit for dev tools

---
+++ console.js
@@ -0,0 +1,12 @@
+let firstNumber = 1;
+let secondNumber = 2;
+
+let answer = firstNumber + secondNumber;
+
+console.log(answer);
+
+console.log( 'the answer is ' + answer);
+
+console.log( 'the answer is', + answer);
+
+console.log( 'the answer is', answer, 'the firstNumber is', firstNumber);
